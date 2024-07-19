0f7afd392d31543dec578f6f64b7411763275887
saived2 <137843759+saived2@users.noreply.github.com>
2024-07-18 20:59:04 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
- 
+loves=0;
+green=0;
+
+function preload() {
+    song = loadSound("loveit.mp3")
+    song = loadSound("greennoise.mp3")
+}
+
+function setup() {
+    canvas = createCanvas(600,500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
